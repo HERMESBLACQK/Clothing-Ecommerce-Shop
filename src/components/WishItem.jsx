d585b20efe4642cfc60ace31cd596fce38d2b819
HermesBlacqk <omotosolilhenxy@gmail.com>
2024-05-03 20:09:17 +0100

    .....

--- src/components/WishItem.jsx
+++ src/components/WishItem.jsx
@@ -7,7 +7,7 @@ import { store } from "../store";
 import { toast } from "react-toastify";
 
 
-const WishItem = ({ item, counter }) => {
+const WishItem = ({ item, counter, image }) => {
     const dispatch = useDispatch();
 
     const removeFromWishlistHandler = async (product) => {
@@ -37,6 +37,12 @@ const WishItem = ({ item, counter }) => {
   return (
     <tr className="hover cursor-pointer">
       <th className="text-accent-content">{ counter + 1 }</th>
+      <th className="text-accent-content">  <img
+            className="rounded-t-lg p-2"
+            src={`https://${image}`}
+            alt="product image"
+          /></th>
+
       <td className="text-accent-content">{ item.title }</td>
       <td className="text-accent-content">{ item.selectedSize }</td>
       <td>
